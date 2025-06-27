a290b15eb53c596901fd4941dc1086ce49c787dd
kami <113427160+kilmane@users.noreply.github.com>
2025-06-27 20:20:07 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="app">
+      <div className="container">
+        <h1 className="title">Ilmane</h1>
+        <div className="decorative-elements">
+          <div className="floating-circle circle-1"></div>
+          <div className="floating-circle circle-2"></div>
+          <div className="floating-circle circle-3"></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
